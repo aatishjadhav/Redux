e7df33729b7108b58df5d103208012bc6972ddb6
Atish <atishjadhav835@gmail.com>
2025-02-24 08:37:29 +0530

    RX1.2_CW_Done

---
+++ RX1.2_CW/script.js
@@ -0,0 +1,38 @@
+import { createStore } from "https://cdn.jsdelivr.net/npm/redux@4.2.1/es/redux.mjs";
+import todosReducer from "./todosReducer.js";
+import { addTodo, removeTodo } from "./actions.js";
+
+const store = createStore(todosReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+  updateTodoList();
+});
+
+const todoInput = document.querySelector("#todoInput");
+const addBtn = document.querySelector("#addTodo");
+const todoList = document.querySelector("#todoList");
+
+const addTodoHandler = () => {
+  const todoValue = todoInput.value;
+  if (todoInput) {
+    store.dispatch(addTodo(todoValue));
+  }
+};
+
+window.removeTodoHandler = (index) => {
+  store.dispatch(removeTodo(index));
+};
+
+addBtn.addEventListener("click", addTodoHandler);
+
+const updateTodoList = () => {
+  const state = store.getState();
+  todoList.innerHTML = state.todos
+    .map((todo, index) => {
+      return `<li>${todo} <button onClick="removeTodoHandler(${index})">Remove</button></li>`;
+    })
+    .join("");
+};
+
+updateTodoList();
