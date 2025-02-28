4501109238757052cff4b23e4d4c35461ff39a3f
Atish <atishjadhav835@gmail.com>
2025-03-01 00:59:22 +0530

    RX3.2_CW_Done

---
+++ RX3.2_CW/src/App.jsx
@@ -0,0 +1,14 @@
+import Posts from "./features/posts/Posts";
+
+function App() {
+  return (
+    <main>
+      <h1>My Social Media App</h1>
+      <div>
+       <Posts/>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ RX3.2_CW/src/features/posts/postSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [
+      {
+        postId: "P001",
+        caption: "Learning Redux Toolkit",
+        likes: 22,
+        user: {
+          userId: "u123",
+          name: "John Doe",
+        },
+      },
+      {
+        postId: "P002",
+        caption: "It is fun to learn redux toolkit",
+        likes: 18,
+        user: {
+          userId: "u123",
+          name: "John Doe",
+        },
+      },
+    ],
+    },
+  reducers: {
+    likeButtonPressed: (state, action) => {
+      const postIndex = state.posts.findIndex((post) => post.postId === action.payload)
+      state.posts[postIndex].likes = state.posts[postIndex].likes + 1;
+    }
+  }
+});
+
+export const { likeButtonPressed } = postSlice.actions;
+
+export default postSlice.reducer;
